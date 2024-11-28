910587a35f5bce8dd6495cbeaa071d98d66ea9cb
Izzulmaaali <ibapak984@gmail.com>
2024-11-28 19:25:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U NJIPAAA').split('')
+    const titles = ('Selamat Malam Unaaa').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
