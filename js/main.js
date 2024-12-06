f22f64e6ca8b07da5908cbd6d9b0124e481b3c41
Izzulmaaali <ibapak984@gmail.com>
2024-12-06 08:37:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Fresh Flowers Buat Unaa :)').split('')
+    const titles = ('HBD Kamuuu, Semangat').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
