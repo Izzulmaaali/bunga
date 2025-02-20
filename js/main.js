a6abcb8f7668816a0fea91039e0b17f3327a5ebd
Izzulmaaali <ibapak984@gmail.com>
2025-02-20 10:33:24 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Bunga Buat zia yang manis').split('')
+    const titles = ('Bunga Buat Kamu hehehe').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
